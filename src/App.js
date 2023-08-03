b6de334faf4069029d34c6d0d0b5fc00abe82788
hemanthiq <hemanthpalukuri55@gmail.com>
2023-08-03 17:10:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  BarChart,
+  XAxis,
+  YAxis,
+  Legend,
+  CartesianGrid,
+  Bar,
+} from "recharts";
 
-function App() {
+const App = () => {
+  const data = [
+    { name: "Facebook", users: 200000000 },
+    { name: "Instagram", users: 500000000 },
+    { name: "Twiter", users: 300000000 },
+    { name: "Telegram", users: 150000000 },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ textAlign: "center" }}>
+      <h1>Socail Media Users</h1>
+      <div className="App">
+        <PieChart width={400} height={400}>
+          <Pie
+            dataKey="users"
+            isAnimationActive={false}
+            data={data}
+            cx={200}
+            cy={200}
+            outerRadius={80}
+            fill="green"
+            label
+          />
+          <Tooltip />
+        </PieChart>
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 80,
+            bottom: 5,
+          }}
+          barSize={30}
         >
-          Learn React
-        </a>
-      </header>
+          <XAxis
+            dataKey="name"
+            scale="point"
+            padding={{ left: 10, right: 10 }}
+          />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Bar dataKey="users" fill="blue" background={{ fill: "#eee" }} />
+        </BarChart>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
